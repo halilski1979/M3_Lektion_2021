8d92606c9b200393f754f0fb1cca13040fee01f0
halil <dospatliiski@gmail.com>
2021-04-13 14:01:37 +0300

    dcsdc

---
+++ 1_Lektion/04_w_2_BankAccaount_Problem2/BankAccount.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_w_2_BankAccaount_Problem2
+{
+    class BankAccount
+    {
+        private int id;
+        private double balance;
+
+        public int ID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public double Balance
+        {
+            get { return balance; }
+            set { balance = value; }
+        }
+
+        public void Deposit(double amount)
+        {
+            balance += amount;
+        }
+
+        public void Withdraw (double amount)
+        {
+            balance -= amount;
+        }
+
+        public override string ToString()
+        {
+            return $"ACCOUNT:{id}   Balance:{balance}";
+        }
+    }
+}
---
+++ 1_Lektion/04_w_2_BankAccaount_Problem2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_w_2_BankAccaount_Problem2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount acc1 = new BankAccount();
+
+            acc1.ID = 1;
+            acc1.Deposit(15);
+            //Добавяме сума към баланса
+            Console.WriteLine(acc1.Balance);
+            //Теглим сума от баланса
+            acc1.Withdraw(10);
+            Console.WriteLine(acc1.Balance);
+
+            Console.WriteLine(acc1);
+        }
+    }
+}
